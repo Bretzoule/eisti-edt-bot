cdf58471d74520a2c9d31b117fdc52125394d49f
Thomas <thomas.lefloch@eisti.fr>
2021-02-27 21:43:56 +0100

    ajout gestion multicalendriers + formattage y/m/d :: h/m/s pour l'affichage + ajout emojis selon le type de cours

--- auth/auth.js
+++ auth/auth.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
@@ -15,16 +15,15 @@ const TOKEN_PATH = 'token.json';
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+function authorize(credentials, callback, message, args) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
+    client_id, client_secret, redirect_uris[0]);
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
+    if (err) return getAccessToken(oAuth2Client, callback, message, args);
     oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+    callback(oAuth2Client, message, args);
   });
 }
 
@@ -34,7 +33,7 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
+function getAccessToken(oAuth2Client, callback, message, args) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
@@ -49,12 +48,11 @@ function getAccessToken(oAuth2Client, callback) {
     oAuth2Client.getToken(code, (err, token) => {
       if (err) return console.error('Error retrieving access token', err);
       oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
         if (err) return console.error(err);
         console.log('Token stored to', TOKEN_PATH);
       });
-      callback(oAuth2Client);
+      callback(oAuth2Client, message, args);
     });
   });
 }
--- index.js
+++ index.js
@@ -1,51 +1,88 @@
 const Discord = require("discord.js");
 const fs = require('fs');
+require('dotenv').config();
 const { google } = require('googleapis');
+const { ml } = require("googleapis/build/src/apis/ml");
 const auth = require('./auth/auth');
 const creds = './auth/credentials.json';
 
-const client = new Discord.Client();
+let calendarArray = {};
+calendarArray['ing1gi1'] = 'c_4c9sl9jjagkgdibl4450u1h038@group.calendar.google.com';
 
-
-const myEmojiArray = [":one:", ":two:", ":three:", ":four:",":five:", ":six:", ":seven:", ":eight:", ":nine:"];
+const tableMois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
+const tableSemaine = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi",];
 
 const myToken = process.env.TOKEN;
+
+const client = new Discord.Client();
 const prefix = 'µ';
 
-fs.readFile(creds, (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  auth.authorize(JSON.parse(content), listEvents);
-});
-
-
-function listEvents(auth) {
+function listEvents(auth, message, args) {
   const calendar = google.calendar({ version: 'v3', auth });
   let today = new Date();
-  let tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate + 1,0,0,0);
-  let schedule = new Discord.MessageEmbed()
-  calendar.events.list({
-    calendarId: 'c_4c9sl9jjagkgdibl4450u1h038@group.calendar.google.com',
-    timeMin: today.toISOString,
-    maxResults: 8,
-    timeMax: tomorrow.toISOString,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('API ERROR : ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Les cours de demain :');
-      schedule.setColor('#0099ff')
-      .setTitle(':books: Emploi du temps des ING1-GI1 :books:')
-      .setDescription('Pour le ' + today.toLocaleDateString)
-      events.map((event, i) => {
-        const start = event.start.dateTime;
-        let myCurrentTime = new Date(start);
-        schedule.addfields({name: myEmojiArray[i] + " : " + myCurrentTime.toTimeString, value: event.summary });
-      });
-    } else {
-      schedule.setTitle("Pas de cours demain bg...").setColor('#0099ff');
-    }
-    message.channel.send(schedule);
-  });
+  // let tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 2, 0, 0, 0);
+  // let nextTomorrow = new Date(tomorrow.getFullYear(), tomorrow.getMonth(), tomorrow.getDate() + 2, 0, 0, 0);
+  let schedule = new Discord.MessageEmbed();
+  let nbElements = ((parseInt(args[args.length - 1], 10) > 0) && (parseInt(args[args.length - 1], 10) < 25)) ? parseInt(args[args.length - 1], 10) : 8;
+  console.log(nbElements);
+  if (args[0] != null && typeof calendarArray[args[0]] !== undefined) {
+    calendarId = calendarArray[args[0]];
+    schedule.setColor('#0099ff')
+      .setTitle(':books: Emploi du temps des ' + args[0] + ' :books:')
+    calendar.events.list({
+      calendarId: calendarId,
+      timeMin: today.toISOString(),
+      maxResults: 15,
+      singleEvents: true,
+      orderBy: 'startTime',
+    }, (err, res) => {
+      if (err) return console.log('API ERROR : ' + err);
+      const events = res.data.items;
+      if (events.length) {
+        schedule.setDescription('Vous avez demandé ' + nbElements + ' items.');
+        events.map((event, i) => {
+          const start = event.start.dateTime;
+          let details = event.summary;
+          let myCurrentTime = new Date(start);
+          emojiToUse = details.endsWith('CM') ? ':notebook:' : details.endsWith('EXAM') ? ':mortar_board:' : ':pencil:';
+          schedule.addFields(
+            {
+              name: emojiToUse + ' : ' + " le " + tableSemaine[myCurrentTime.getDay()] + " " + myCurrentTime.getUTCDate() + " " + tableMois[myCurrentTime.getMonth()] + ' à ' + myCurrentTime.getUTCHours() + ":" + myCurrentTime.getUTCMinutes(),
+              value: details
+            }
+          );
+        });
+      } else {
+        schedule.setDescription("Pas de cours demain bg.");
+      }
+      message.channel.send(schedule);
+    });
+  } else {
+    message.channel.send("Merci de préciser un emploi du temps valide à afficher, tapez µhelp pour avoir la liste.");
+  }
 }
+
+
+client.on('ready', () => {
+  console.log("I'm on");
+  client.user.setActivity("µedt pour avoir l'emploi du temps !");
+})
+
+client.on('message', async message => {
+  if (!message.content.startsWith(prefix) || message.author.bot || message.channel.type === "dm") return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+  switch (command) {
+    case 'edt':
+      fs.readFile(creds, (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        auth.authorize(JSON.parse(content), listEvents, message, args);
+      });
+      break;
+    case 'help':
+
+      break;
+  }
+});
+
+client.login(myToken).catch(err => console.log(err));
